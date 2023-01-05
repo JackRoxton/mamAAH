f07a48537fcd9ce97b2a6bc9fee73d4623aea294
QuentinLamy <quentin.lamy16@gmail.com>
2023-01-05 10:02:18 +0100

    console, prefab

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour
+{
+
+    bool state = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SwitchState()
+    {
+        if (state)
+        {
+            Debug.Log("consoleOff");
+            this.GetComponent<SpriteRenderer>().color = Color.black;
+            state = false;
+        }
+        else
+        {
+            Debug.Log("consoleOn");
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+            state = true;
+        }
+    }
+
+}
