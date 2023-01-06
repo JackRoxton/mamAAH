2f67793d6f8204ffcd25c72ff021cbee556f12c2
oct0punk <elric.emile@gmail.com>
2023-01-06 11:15:21 +0100

    MAJ monstre et TV

--- Assets/Scripts/Monster/Monster_script.cs
+++ Assets/Scripts/Monster/Monster_script.cs
@@ -10,7 +10,9 @@ public class Monster_script : MonoBehaviour
     [SerializeField] private GameObject nextToTheBed;
     [SerializeField] private GameObject OnTheBed;
     [SerializeField] private GameObject TV;
+    [SerializeField] private Light TVLight;
     [SerializeField] private AnimationCurve scaleCurve;
+    [SerializeField] private AnimationCurve TVcurve;
 
     [HideInInspector] public float advance = 0; // From 0 to 3
     private float advanceRate = .6f;
@@ -31,31 +33,36 @@ public class Monster_script : MonoBehaviour
         {
             transform.position = Vector3.Lerp(underTheBed.transform.position, nextToTheBed.transform.position, advance);
             transform.localScale = Vector3.one;
+            TV.GetComponent<SpriteRenderer>().color = Color.black;
         }
         else if (advance < 2)
         {
             transform.position = Vector3.Lerp(nextToTheBed.transform.position, OnTheBed.transform.position, advance - 1);
             transform.localScale = Vector3.one;
-            //TV.GetComponent<SpriteRenderer>().color = Color.black;
+            TV.GetComponent<SpriteRenderer>().color = Color.black;
         }
         else
         {
-            //TV.GetComponent<SpriteRenderer>().color = Color.white;
+            float t = TVcurve.Evaluate(advance - 2);
+            float r = 1 - t;
+            TVLight.intensity = TVcurve.Evaluate(advance - 2) * 3;
+            TV.GetComponent<SpriteRenderer>().color = new Color(t + r * .1f, t, t);
             AudioManager.Instance.PlayScratching();
             transform.localScale = Vector3.one * scaleCurve.Evaluate(advance - 2);
             Vector3 minusZ = transform.position;
-            minusZ.z = -.9f;
+            minusZ.z = Mathf.Lerp(-.9f, -2, scaleCurve.Evaluate(advance - 2) * 100);
+            Debug.Log(advance - 2);
             transform.position = minusZ;
             if (advance >= 3) GameManager.Instance.GameOverMonster();
             PostProcessSript.Instance.vignetteValue = (advance - 2);
-        }        
+        }
     }
 
     public void UpdateLight(bool val)
     {
         if (mumIsHere) return;
         advanceTarget = val ? 1 : 3;
-        advanceRate = .3f;
+        advanceRate = .2f;
     }
 
     public void MumIsComing(bool val)
